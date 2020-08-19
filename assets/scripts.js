4bbcee1c6ce64c5ee35acd35f700725f96fb6da2
Neil Thawani <neilthawani@gmail.com>
2020-08-19 15:59:37 -0500

    Small refactor

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,123 +1,52 @@
-// https://codepen.io/yananas/pen/rwvZvY
-
-document.addEventListener("DOMContentLoaded", function(event) {
-    // initalize
-    var body = document.getElementsByTagName("body")[0];
-    var canvas = document.createElement("canvas");
-    var ctx = canvas.getContext("2d");
-    var linesArray = [];
-
+document.addEventListener("DOMContentLoaded", function() {
+    // SETTING ALL VARIABLES
     var isMouseDown = false;
-
-    currentSize = 1;
-
-    var currentColor = "red";
+    var canvas = document.createElement('canvas');
+    var body = document.getElementsByTagName("body")[0];
+    var ctx = canvas.getContext('2d');
+    var linesArray = [];
+    currentSize = 5;
+    var currentColor = "rgb(200,20,100)";
     var currentBg = "white";
 
-    // create canvas
-    ctx.fillStyle = currentBg;
-    console.log("create canvas, ctx.fillStyle", ctx.fillStyle);
-    canvas.width = 800;
-    canvas.height = 600;
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
-    // redraw();
-    document.getElementsByClassName("canvas-container")[0].appendChild(canvas);
+    // INITIAL LAUNCH
 
-    // drawing event handlers
-    canvas.addEventListener("mousedown", function(event) {
-        // console.log("mousedown", this, event);
-        isMouseDown = true;
-
-        var currentPosition = getMouseCoords(this, event);
-        ctx.moveTo(currentPosition.x, currentPosition.y)
-        ctx.beginPath();
-        ctx.lineWidth = currentSize;
-        ctx.lineCap = "round";
-        ctx.strokeStyle = currentColor;
-    });
-
-    canvas.addEventListener("mousemove", function(event) {
-        console.log("isMouseDown", isMouseDown);
-        if (isMouseDown) {
-            var currentPosition = getMouseCoords(canvas, event);
-            // console.log("currentPosition", currentPosition);
-            ctx.lineTo(currentPosition.x, currentPosition.y)
-            ctx.stroke();
-            storeData(currentPosition.x, currentPosition.y, currentSize, currentColor);
-        }
-    });
-
-    function storeData(x, y, size, color) {
-        var line = {
-            "x": x,
-            "y": y,
-            "size": size,
-            "color": color
-        }
-        linesArray.push(line);
-    }
-
-    function getMouseCoords(canvas, event) {
-        var rect = canvas.getBoundingClientRect();
-
-        return {
-            x: event.clientX - rect.left,
-            y: event.clientY - rect.top
-        };
-    }
-
-    canvas.addEventListener("mouseup", function(event) {
-        isMouseDown = false;
-    });
-
-    // eraser event handler
-    document.getElementById("eraser").addEventListener("click", function(event) {
-        currentSize = 50;
-        currentColor = ctx.fillStyle
-        console.log("eraser currentColor", currentColor);
-    });
-
-    // change draw color
-    document.getElementById("draw-colorpicker").addEventListener("change", function() {
-        currentColor = this.value;
-    });
-
-    // change background color
-    document.getElementById("background-colorpicker").addEventListener("change", function() {
-        ctx.fillStyle = this.value;
-        console.log("background-colorpicker ctx.fillStyle", ctx.fillStyle);
-        ctx.fillRect(0, 0, canvas.width, canvas.height);
-        // redraw();
-        currentBg = ctx.fillStyle;
-    });
-});
+    createCanvas();
 
     // BUTTON EVENT HANDLERS
 
-    // document.getElementById("canvasUpdate").addEventListener("click", function() {
+    // document.getElementById('canvasUpdate').addEventListener('click', function() {
     //     createCanvas();
     //     redraw();
     // });
-
-    // document.getElementById("controlSize").addEventListener("change", function() {
+    document.getElementById('draw-colorpicker').addEventListener('change', function() {
+        currentColor = this.value;
+    });
+    document.getElementById('background-colorpicker').addEventListener('change', function() {
+        ctx.fillStyle = this.value;
+        ctx.fillRect(0, 0, canvas.width, canvas.height);
+        redraw();
+        currentBg = ctx.fillStyle;
+    });
+    // document.getElementById('controlSize').addEventListener('change', function() {
     //     currentSize = this.value;
     //     document.getElementById("showSize").innerHTML = this.value;
     // });
-
-    // document.getElementById("saveToImage").addEventListener("click", function() {
-    //     downloadCanvas(this, "canvas", "masterpiece.png");
+    // document.getElementById('saveToImage').addEventListener('click', function() {
+    //     downloadCanvas(this, 'canvas', 'masterpiece.png');
     // }, false);
-
-    // document.getElementById("clear").addEventListener("click", createCanvas);
-    // document.getElementById("save").addEventListener("click", save);
-    // document.getElementById("load").addEventListener("click", load);
-    // document.getElementById("clearCache").addEventListener("click", function() {
+    document.getElementById('eraser').addEventListener('click', eraser);
+    // document.getElementById('clear').addEventListener('click', createCanvas);
+    // document.getElementById('save').addEventListener('click', save);
+    // document.getElementById('load').addEventListener('click', load);
+    // document.getElementById('clearCache').addEventListener('click', function() {
     //     localStorage.removeItem("savedCanvas");
     //     linesArray = [];
     //     console.log("Cache cleared!");
     // });
 
     // REDRAW
+
     function redraw() {
         for (var i = 1; i < linesArray.length; i++) {
             ctx.beginPath();
@@ -128,6 +57,30 @@ document.addEventListener("DOMContentLoaded", function(event) {
             ctx.lineTo(linesArray[i].x, linesArray[i].y);
             ctx.stroke();
         }
+    }
+
+    // DRAWING EVENT HANDLERS
+
+    canvas.addEventListener('mousedown', function() {
+        mousedown(canvas, event);
+    });
+    canvas.addEventListener('mousemove', function() {
+        mousemove(canvas, event);
+    });
+    canvas.addEventListener('mouseup', mouseup);
+
+    // CREATE CANVAS
+
+    function createCanvas() {
+        canvas.id = "canvas";
+        canvas.width = 800; // parseInt(document.getElementById("sizeX").value);
+        canvas.height = 600; // parseInt(document.getElementById("sizeY").value);
+        canvas.style.zIndex = 8;
+        canvas.style.position = "absolute";
+        canvas.style.border = "1px solid";
+        ctx.fillStyle = currentBg;
+        ctx.fillRect(0, 0, canvas.width, canvas.height);
+        body.appendChild(canvas);
     }
 
     // DOWNLOAD CANVAS
@@ -168,30 +121,63 @@ document.addEventListener("DOMContentLoaded", function(event) {
 
     // ERASER HANDLING
 
+    function eraser() {
+        currentSize = 50;
+        currentColor = ctx.fillStyle
+    }
+
     // GET MOUSE POSITION
 
+    function getMousePos(canvas, evt) {
+        var rect = canvas.getBoundingClientRect();
+        return {
+            x: evt.clientX - rect.left,
+            y: evt.clientY - rect.top
+        };
+    }
 
+    // ON MOUSE DOWN
+
+    function mousedown(canvas, evt) {
+        var mousePos = getMousePos(canvas, evt);
+        isMouseDown = true
+        var currentPosition = getMousePos(canvas, evt);
+        ctx.moveTo(currentPosition.x, currentPosition.y)
+        ctx.beginPath();
+        ctx.lineWidth = currentSize;
+        ctx.lineCap = "round";
+        ctx.strokeStyle = currentColor;
+
+    }
+
+    // ON MOUSE MOVE
+
+    function mousemove(canvas, evt) {
+
+        if (isMouseDown) {
+            var currentPosition = getMousePos(canvas, evt);
+            ctx.lineTo(currentPosition.x, currentPosition.y)
+            ctx.stroke();
+            store(currentPosition.x, currentPosition.y, currentSize, currentColor);
+        }
+    }
 
     // STORE DATA
 
-
+    function store(x, y, s, c) {
+        var line = {
+            "x": x,
+            "y": y,
+            "size": s,
+            "color": c
+        }
+        linesArray.push(line);
+    }
 
     // ON MOUSE UP
 
-    // function mouseup() {
-    //     isMouseDown = false;
-        // storeData()
-    // }
-// });
-
-// display the lines/drawing - DONE
-// add an eraser:
-  // The eraser acts like a smudge eraser.
-  // When a user smudges over a line in eraser mode, that erases the entire line.
-  // When a user points and clicks a line, it erases.
-// bonus: sync with firebase for CRDT functionality
-// bonus: Add other tools like colors, highlighting, photos, or something else!
-// bonus: draw bezier curves
-// share the repo with jobs@classkick.com and pro@classkick.com
-// email this doc to jobs@classkick.com
-// https://docs.google.com/document/d/1oZMvdSoxhI4Ui_xGcbve5qKTcMTdor159stq_Rt9_ZY/edit#heading=h.jd7duor6jz70
+    function mouseup() {
+        isMouseDown = false
+        store()
+    }
+});
